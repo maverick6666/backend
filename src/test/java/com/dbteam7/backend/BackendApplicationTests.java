b75e3007dd9c944f24f323b6a42c1017714fc5c4
장원진 <jjwj0228@naver.com>
2025-11-10 16:09:20 +0900

    chore: 백엔드 기본세팅

---
+++ src/test/java/com/dbteam7/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbteam7.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
